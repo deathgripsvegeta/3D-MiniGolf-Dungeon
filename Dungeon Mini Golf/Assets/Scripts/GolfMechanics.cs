4e692e4c5d11b97d50557693dad06a1e0871d1e2
deathgripsvegeta <sophiahowell831@gmail.com>
2023-05-01 11:32:05 -0700

    aaaaaa

---
+++ Dungeon Mini Golf/Assets/Scripts/GolfMechanics.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GolfMechanics : MonoBehaviour
+{
+    [SerializeField] public GameObject Ball; 
+    
+    [SerializeField] public Slider hitForceSlider;
+    [SerializeField] public float hitForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
